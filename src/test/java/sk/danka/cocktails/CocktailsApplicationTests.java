47f8674be268561a2e97a0a9fead09fa7c0a0762
Daniela Kriss Mrozova <daniela.kriss.mrozova@uniquepeople.net>
2021-02-11 14:55:57 +0100

    Spring Initializer

---
+++ src/test/java/sk/danka/cocktails/CocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.danka.cocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
